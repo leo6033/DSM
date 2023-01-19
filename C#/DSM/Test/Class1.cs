44566b713bb7c58f942b15fc9ba43f3b6278b42f
leo6033 <long452a@163.com>
2023-01-19 17:01:39 +0800

    修改文件目录，添加Test

---
+++ C#/DSM/Test/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Disc0ver.Gameplay.StateMachine.Example;
+
+namespace Test
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.Init();
+
+            while (true)
+            {
+                controller.Tick();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
