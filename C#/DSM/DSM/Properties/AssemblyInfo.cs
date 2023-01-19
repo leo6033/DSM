e6bd10a8d2bde55cc637ddf97dabefc7440feac4
leo6033 <long452a@163.com>
2023-01-19 17:15:55 +0800

    修改文件目录

--- C#/DSM/DSM/Properties/AssemblyInfo.cs
+++ C#/DSM/DSM/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("com.disc0ver.DSM")]
+[assembly: AssemblyTitle("DSM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("com.disc0ver.DSM")]
+[assembly: AssemblyProduct("DSM")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
